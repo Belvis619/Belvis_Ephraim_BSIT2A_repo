ae36b55886f008bd12aea53d461b004a40b21641
Belvis619 <ephraimbelvis@filamer.edu.ph>
2025-03-24 16:44:53 +0800

    ephraim

---
+++ Activities/Function-Activity/script.js
@@ -0,0 +1,11 @@
+function whatShallWear (temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    }
+    
+    else if (temp < 70) {
+        console.log("Wear a sweater");
+    }
+
+    
+}
